c2b395abfd9081ea4ae1ba3c514a7f8e26a32f91
Adam Harazim <ah@rheinfabrik.de>
2015-05-21 17:29:20 +0200

    test

---
+++ app/src/androidTest/java/de/rheinfabrik/mygesturescrollview/SimpleTest.java
@@ -0,0 +1,19 @@
+package de.rheinfabrik.mygesturescrollview;
+
+import android.test.InstrumentationTestCase;
+
+/**
+ * Created by Adam on 21.05.15.
+ */
+public class SimpleTest extends InstrumentationTestCase {
+
+    public void test1and1() {
+
+        int i = 0;
+
+        i = 1;
+
+        assertEquals(i, 0);
+    }
+
+}
